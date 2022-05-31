8bbc91d0ef4a21d953298baa3f0df46ba991b857
KenanNagiyev <788lnmp@code.edu.az>
2022-05-31 22:01:16 +0400

    new file

---
+++ tasknest/Migrations/20220531165653_SliderTable.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace tasknest.Migrations
+{
+    public partial class SliderTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ tasknest/ViewModels/HomeVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using tasknest.Models;
+
+namespace tasknest.ViewModels
+{
+    public class HomeVM
+    {
+        public List<Slider> Sliders { get; set; }
+        public List<Category> Categories { get; set; }
+        public List<Product> Products { get; set; }
+        public List<Product> RecentProducts { get; set; }
+        public List<Product> TopRatedProducts { get; set; }
+    }
+}
